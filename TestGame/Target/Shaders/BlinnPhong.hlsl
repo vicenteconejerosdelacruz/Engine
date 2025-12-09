4ef2200e13810852775d3e417127fb7fd7d4064e
vicenteconejerosdelacruz <vicenteconejeros@gmail.com>
2025-12-08 23:59:26 -0300

    assets of gangmember

--- TestGame/Target/Shaders/BlinnPhong.hlsl
+++ TestGame/Target/Shaders/BlinnPhong.hlsl
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:23c14d276e183ff5c19a6e35f61781a9879a6b57b0eeb9ec996bc37ac7b1bc0a
-size 3564
+oid sha256:4b8c20783d800b6405468e050abc04a0178dc74609736a3840979dc625a27e97
+size 4499
