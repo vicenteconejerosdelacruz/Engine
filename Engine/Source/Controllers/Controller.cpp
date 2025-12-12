5355fb720113399f2d6a879712443651f78f6f07
vicenteconejerosdelacruz <vicenteconejeros@gmail.com>
2025-12-12 19:44:49 -0300

    fixed crash when the controller was already destroyed

--- Engine/Source/Controllers/Controller.cpp
+++ Engine/Source/Controllers/Controller.cpp
@@ -52,6 +52,7 @@ namespace Game
 
 	void DestroyController(JUUID uuid)
 	{
+		if (!controllersUUIDs.contains(uuid)) return;
 		controllersUUIDs.at(uuid)->Unmap();
 		controllersUUIDs.erase(uuid);
 		for (auto it = controllerUUIDsByName.begin(); it != controllerUUIDsByName.end();)
