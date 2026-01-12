ccc6a19fe12348a1084ff34b2316c34eac2c254d
vicenteconejerosdelacruz <vicenteconejeros@gmail.com>
2026-01-12 11:11:07 -0300

    adding ContextController function template

--- Engine/Source/Controllers/Controller.h
+++ Engine/Source/Controllers/Controller.h
@@ -40,4 +40,24 @@ namespace Game
 
 	extern std::vector<std::string> GetControllers();
 	extern JUUID CreateController(std::string name, SUUUID sceneObject, nlohmann::json& json);
+
+	template<typename T>
+	T* ContextController()
+	{
+		v8::Isolate* isolate = v8::Isolate::GetCurrent();
+		v8::Local<v8::Context> context = isolate->GetCurrentContext();
+
+		v8::Context::Scope context_scope(context);
+		v8::HandleScope handle_scope(isolate);
+
+		v8::Local<v8::Object> global = context->Global();
+
+		v8::Local<v8::String> key = v8::String::NewFromUtf8(isolate, "uuid", v8::NewStringType::kNormal).ToLocalChecked();
+		v8::Local<v8::Value> value_js = global->Get(context, key).ToLocalChecked();
+		v8::String::Utf8Value utf8_value(isolate, value_js);
+		std::string uuid(*utf8_value);
+
+		T* controller = static_cast<T*>(GetController(uuid).get());
+		return controller;
+	}
 };
